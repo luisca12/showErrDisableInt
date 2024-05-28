3e478f908e34dfdfd836108fd2391b6d53d843de
luisca12 <114428487+luisca12@users.noreply.github.com>
2024-05-28 14:14:47 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,34 @@
+from auth import Auth
+from strings import menuString, greetingString, inputErrorString
+from functions import checkIsDigit, mkdir
+from commandsCLI import errDisable
+from log import logConfiguration
+import os
+import logging.config
+
+def main():
+    os.system("CLS")
+    mkdir()
+    greetingString()
+    logging.config.dictConfig(logConfiguration)
+    authLog = logging.getLogger('infoLog')
+    validIPs, username, netDevice = Auth()
+
+    while True:
+        menuString(validIPs, username), print("\n")
+        selection = input("Please choose the option that yyou want: ")
+        if checkIsDigit(selection):
+            if selection == "1":
+                # This option will fix errDisable interfaces
+                errDisable(validIPs, username, netDevice)
+            if selection == "2":
+                authLog.info(f"User {username} disconnected from the devices {validIPs}")
+                authLog.info(f"User {username} logged out from the program.")
+                break
+        else:
+            authLog.error(f"Wrong option chosen {selection}")
+            inputErrorString()
+            os.system("PAUSE")
+
+if __name__ == "__main__":
+    main()
